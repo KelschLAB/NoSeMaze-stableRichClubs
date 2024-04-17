588db30a4232e62bff31644a7012066ecdd17b61
Corentin Nélias <corentin.nelias@gmail.com>
2024-04-17 18:22:58 +0200

    annotated data

--- scripts/bootstrap_pop.py
+++ scripts/bootstrap_pop.py
@@ -2,21 +2,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-number_of_hits = []
-shuffled_arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-for i in tqdm(range(10000)):
-    hits = 0
-    for k in range(10):
-        for j in range(5):    
-            np.random.shuffle(shuffled_arr)
-            # if shuffled_arr[0] == 1 or shuffled_arr[0] == 2 or shuffled_arr[0] == 3: #for curated  cohort
-            if shuffled_arr[0] == 1 or shuffled_arr[0] == 2 or shuffled_arr[1] == 1 or shuffled_arr[1] == 2\
-                or shuffled_arr[2] == 1 or shuffled_arr[2] == 2 or shuffled_arr[3] == 1 or shuffled_arr[3] == 2: #for control cohort
-                hits += 1
-    number_of_hits.append((100*hits)/50)
+def mutant_in_cohort():
+    number_of_hits = []
+    shuffled_arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     
-h = plt.hist(number_of_hits, bins = 25, density = True)
-plt.title("Random chance of mutant in rich-club\n Control cohort")
-plt.xlabel("Observed percentage", fontsize=15)
-plt.ylabel("Probability of observation", fontsize=15)
+    for i in tqdm(range(10000)):
+        hits = 0
+        for k in range(10):
+            for j in range(5):    
+                np.random.shuffle(shuffled_arr)
+                if shuffled_arr[0] == 1 or shuffled_arr[0] == 2 or shuffled_arr[0] == 3: #for curated  cohort
+                    hits += 1
+        number_of_hits.append((100*hits)/50)
+        
+    h = plt.hist(number_of_hits, bins = 25, density = True)
+    plt.title("Random chance of mutant in rich-club\n Control cohort")
+    plt.xlabel("Observed percentage", fontsize=15)
+    plt.ylabel("Probability of observation", fontsize=15)
+
+
+def mutants_in_validation():
+    number_of_hits = []
+    mutants = np.array([1, 2, 3, 4])
+    shuffled_arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    
+    for i in tqdm(range(10000)):
+        hits = 0
+        # expID 11
+        np.random.shuffle(shuffled_arr)
+        hits += np.sum(np.isin(mutants, shuffled_arr[:5]))
+        # expID 12
+        np.random.shuffle(shuffled_arr)
+        hits += np.sum(np.isin(mutants, shuffled_arr[:4]))
+        # expID 14
+        np.random.shuffle(shuffled_arr)
+        hits += np.sum(np.isin(mutants, shuffled_arr[:4]))
+        # expID 15
+        np.random.shuffle(shuffled_arr)
+        hits += np.sum(np.isin(mutants, shuffled_arr[:3]))
+        # expID 17
+        np.random.shuffle(shuffled_arr)
+        hits += np.sum(np.isin(mutants, shuffled_arr[:6]))
+            
+        number_of_hits.append((100*hits)/(22))
+        
+    h = plt.hist(number_of_hits, bins = 30, density = True)
+    plt.axvline(np.percentile(number_of_hits, 5), ls = "--", color = 'k')
+    plt.axvline(np.percentile(number_of_hits, 95), ls = "--", color = 'k', label = "95% CI")
+    plt.legend()
+    plt.title("Random chance of mutant in rich-club\n Control cohort")
+    plt.xlabel("Percentage of mutants in RC", fontsize=15)
+    plt.ylabel("Probability of observation", fontsize=15)
+
+
+mutants_in_validation()
--- scripts/histogram_richclub.py
+++ scripts/histogram_richclub.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def tuberank_vs_rc():
     arr = np.array([2, 3, 5, 1, 2, 4, 1, 7, 4, 2, 2, 1, 1, 6, 4, 6, 5, 1, 2, 3]) # chasing rank
     arr = np.array([2, 4, 2, 3, 8, 2, 10, 1, 8, 2, 6, 5, 3, 1, 2, 5, 1, 2, 6, 2, 4]) # tube rank
@@ -42,7 +43,6 @@ def chasings_vs_rc():
     rects2 = ax.bar(x - width/3, 100*arr30/tot30, 2*width/3, label='30\%', align = 'edge', color = colors[1])
     rects3 = ax.bar(x + width/3, 100*arr50/tot50, 2*width/3, label='50\%', align = 'edge', color = colors[2]) 
 
-     
     ax.spines[['right', 'top']].set_visible(False)
     ax.set_ylabel('Total chasings fraction (\%)')
     ax.set_xlabel('Cohort')
@@ -56,6 +56,56 @@ def chasings_vs_rc():
     plt.savefig("C:\\Users\\Agarwal Lab\\Corentin\\Python\\clusterGUI\\plots\\chasings_vs_RC.png", dpi = 150)
     plt.show()
     
+def chasings_vs_rc_validation():
+    tot10 = np.array([31, 54, np.nan, 42, 38, np.nan,  62]) # total chasing from RC for thres 10%
+    tot30 = np.array([17, 15, np.nan, 19, 19, np.nan,  22]) # total chasing from RC for thres 30%
+    tot50 = np.array([7, 8, np.nan, 8, 11, np.nan,  5]) # chasing from RC for thres 50%
+    tot70 = np.array([3, 4, np.nan, 3, 3, np.nan,  1]) # chasing from RC for thres 70%
+    arr10 = np.array([27, 31, np.nan, 31, 21, np.nan,  46]) # chasing from RC for thres 10%
+    arr30 = np.array([17, 14, np.nan, 17, 11, np.nan,  15]) # chasing from RC for thres 30%
+    arr50 = np.array([7, 8, np.nan, 8, 6, np.nan,  3]) # chasing from RC for thres 50%
+    arr70 = np.array([3, 4, np.nan, 3, 2, np.nan,  1]) # chasing from RC for thres 70%
+
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Helvetica"
+    })
+
+    labels = ['G11', 'G12', 'G13', 'G14', 'G15', 'G16', 'G17']
+    x = np.arange(len(labels))*1
+    width = 0.5
+    fig, ax = plt.subplots()
+    
+    # colors = ["#ffeda0", "#feb24c", "#f03b20"] # reds
+    colors = ["#edf8b1", "#7fcdbb", "#2c7fb8", "#225ea8"] # veridis
+    # colors = ['#f0f0f0','#bdbdbd','#636363'] # grays
+    # colors = ['#e0ecf4','#9ebcda','#8856a7'] # purples
+    # colors = ['#a1dab4','#41b6c4','#225ea8'] # veridis dark
+    
+    rects1 = ax.bar(x - width/2, 100*arr10/tot10, width/4, label='10\%', align = 'edge', color = colors[0]) 
+    rects2 = ax.bar(x - width/4, 100*arr30/tot30, width/4, label='30\%', align = 'edge', color = colors[1])
+    rects3 = ax.bar(x, 100*arr50/tot50, width/4, label='50\%', align = 'edge', color = colors[2]) 
+    rects4 = ax.bar(x + width/4, 100*arr70/tot70, width/4, label='70\%', align = 'edge', color = colors[3]) 
+    ax.annotate('No rich-club', xy=(2, 30), xytext=(2, 25), rotation = 90)      # 25 because there are 2 to 3 members of stable rich club
+    ax.annotate('No rich-club', xy=(5, 30), xytext=(5, 25), rotation = 90)      # 25 because there are 2 to 3 members of stable rich club
+
+
+    ax.spines[['right', 'top']].set_visible(False)
+    ax.set_ylabel('Total chasings fraction (\%)')
+    ax.set_xlabel('Cohort')
+    ax.set_title('Rich-club members chasings, validation cohort')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend(title="Threshold:")
+    # ax.hlines(40, x[0] - width, x[-1] + width, linestyle = "--", color = "k", alpha = 0.5)  # 25 because there are 2 to 3 members of stable rich club
+    # ax.annotate('Random chance', xy=(5,45), xytext=(4.2 + width/10, 70), ha = 'left',      # 25 because there are 2 to 3 members of stable rich club
+    #         arrowprops=dict(facecolor='black', shrink=0.05, width = 0.5, headwidth = 5), textcoords='data', xycoords='data')
+    plt.savefig("C:\\Users\\Agarwal Lab\\Corentin\\Python\\clusterGUI\\plots\\chasings_vs_RC_validation.png", dpi = 150)
+    plt.show()
+    
 chasings_vs_rc()
+# chasings_vs_rc_validation()
 
     
