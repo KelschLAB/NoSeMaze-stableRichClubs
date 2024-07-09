9974d77c80daac233620832fce59c62efee7f57d
Corentin Nélias <corentin.nelias@gmail.com>
2024-07-09 17:11:27 +0200

    New script for counting chasings

---
+++ scripts/chasings_counting.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import igraph as ig
+import networkx as nx
+import os
+from weighted_rc import weighted_rich_club
+
+plt.rcParams.update({"text.usetex": True})
