bbf60e6d37be0e8612e7d2daa017d091fe407201
Corentin Nélias <corentin.nelias@gmail.com>
2024-07-25 13:14:42 +0200

    permutation test

--- scripts/bootstrap_pop.py
+++ scripts/bootstrap_pop.py
@@ -152,10 +152,95 @@ def mutants_in_validation():
     plt.xlabel("Groups with at least 1 mutants in RC", fontsize=15)
     plt.ylabel("Probability", fontsize=15)
     plt.legend()
+    
+    
+def littermates_in_club():
+    number_of_hits = []
+    shuffled_arr1 = np.array([41, 31, 2, 3, 3, 5, 6, 7, 7, 8])
+    # shuffled_arr2 = np.array([25, 28, 40, 41, 42, 2, 5, 6, 8])
+    shuffled_arr3 = np.array([25, 29, 29, 29, 32, 32, 2, 3, 4])
+    shuffled_arr4 = np.array([25, 31, 2, 3, 5, 5, 6, 7, 7, 8])
+    shuffled_arr5 = np.array([25, 28, 29, 29, 30, 42, 2, 3, 6])
+    shuffled_arr6 = np.array([25, 29, 41, 4, 5, 5, 6, 7, 8, 8])
+    shuffled_arr7 = np.array([25, 25, 29, 29, 32, 32, 2, 3, 5])
+    shuffled_arr8 = np.array([25, 28, 29, 40, 41, 2, 5, 6, 8]) 
+    shuffled_arr10 = np.array([28, 29, 40, 41, 42, 2, 5, 5, 6])
+    shuffled_arr11 = np.array([13, 14, 14, 15, 15, 15, 17, 17, 18, 22])
+    shuffled_arr12 = np.array([11, 12, 13, 14, 15, 20, 21, 23, 23, 23])
+    shuffled_arr15 = np.array([13, 14, 14, 15, 15, 15, 16, 17, 17, 22])
+    shuffled_arr17 = np.array([10, 13, 19, 20, 21, 21, 22, 23, 23, 23])
 
+
+    for i in tqdm(range(1000)):
+        hits = 0
+        # expID 1
+        np.random.shuffle(shuffled_arr1)
+        if shuffled_arr1[0] == shuffled_arr1[1]:
+            hits += 1
+        # expID 2
+        # np.random.shuffle(shuffled_arr2)
+        # if shuffled_arr2[0] == shuffled_arr2[1] or shuffled_arr2[1] == shuffled_arr2[2] or shuffled_arr2[0] == shuffled_arr2[2]:
+        #     hits += 1
+            
+        np.random.shuffle(shuffled_arr3)
+        if shuffled_arr3[0] == shuffled_arr3[1] or shuffled_arr3[1] == shuffled_arr3[2] or shuffled_arr3[0] == shuffled_arr3[2]:
+            hits += 1
+        # expID 4
+        np.random.shuffle(shuffled_arr4)
+        if shuffled_arr4[0] == shuffled_arr4[1]:
+            hits += 1
+        # expID 5
+        np.random.shuffle(shuffled_arr5)
+        if shuffled_arr5[0] == shuffled_arr5[1]:
+            hits += 1
+        # expID 6
+        np.random.shuffle(shuffled_arr6)
+        if shuffled_arr6[0] == shuffled_arr6[1]:
+            hits += 1
+        # expID 7
+        np.random.shuffle(shuffled_arr7)
+        if shuffled_arr3[0] == shuffled_arr7[1] or shuffled_arr7[1] == shuffled_arr7[2] or shuffled_arr7[0] == shuffled_arr7[2]:
+            hits += 1
+        # expID 8
+        np.random.shuffle(shuffled_arr8)
+        if shuffled_arr8[0] == shuffled_arr8[1]:
+            hits += 1
+        # expID 10
+        np.random.shuffle(shuffled_arr10)
+        if shuffled_arr10[0] == shuffled_arr10[1]:
+            hits += 1
+            
+        np.random.shuffle(shuffled_arr11)
+        if shuffled_arr11[0] == shuffled_arr11[1]:
+            hits += 1
+
+        np.random.shuffle(shuffled_arr12)
+        if shuffled_arr12[0] == shuffled_arr12[1]:
+            hits += 1
+            
+        np.random.shuffle(shuffled_arr15)
+        if shuffled_arr15[0] == shuffled_arr15[1] or shuffled_arr15[1] == shuffled_arr15[2] or shuffled_arr15[0] == shuffled_arr15[2]:
+            hits += 1
+        
+        np.random.shuffle(shuffled_arr17)
+        if shuffled_arr17[0] == shuffled_arr17[1]:
+                hits += 1
+
+        number_of_hits.append(hits)
+        
+    h = plt.hist(number_of_hits, bins = np.arange(10), density = True, align = 'left', label = "Expected by random chance", rwidth = 0.95, color = 'gray')
+    plt.title("Random chance of litter mates in rich club")
+    plt.xlabel("Groups with litter mates in rich club", fontsize=15)
+    plt.ylabel("Probability", fontsize=15)
+    plt.axvline(np.percentile(number_of_hits, 2.5), ls = "--", color = 'k')
+    plt.axvline(np.percentile(number_of_hits, 98), ls = "--", color = 'k', label = "95% CI")
+    plt.axvline(3, color = 'red', label = "Experimentally observed")
+    plt.legend()
+
+littermates_in_club()
 # mutant_in_cohort()
-for i in range(1):
-    plt.figure()
+# for i in range(1):
+    # plt.figure()
     # mutant_in_cohort()
-    mutants_in_validation()
+    # mutants_in_validation()
     # mutants_in_both()
