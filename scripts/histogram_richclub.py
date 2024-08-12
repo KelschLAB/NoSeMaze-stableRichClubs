9ef164ab156663e84239eb916b0978726cee87d1
Corentin Nélias <corentin.nelias@gmail.com>
2024-08-12 13:59:18 +0200

    both cohorts in ranking

--- scripts/histogram_richclub.py
+++ scripts/histogram_richclub.py
@@ -137,16 +137,25 @@ def total_chasings_cohort():
 
 
 def tuberank_vs_rc():
-    arr = np.array([2, 3, 5, 1, 2, 4, 1, 7, 4, 2, 2, 1, 1, 6, 4, 6, 5, 1, 2, 3]) # chasing rank
-    arr = np.array([2, 4, 2, 3, 8, 2, 10, 1, 8, 2, 6, 5, 3, 1, 2, 5, 1, 2, 6, 2, 4]) # tube rank
+    """Plots the tube rank of rich clulb members for both cohorts"""
+    arr = np.array([2, 4, 2, 3, 8, 2, 10, 1, 8, 2, 6, 5, 3, 1, 2, 5, 1, 2, 6, 2, 4, 2, 3, 1, 2, 1, 4, 2, 3, 1, 2]) # tube rank
     plt.hist(arr, bins = [i for i in range(1, 11)], align = 'mid', rwidth = 0.95, color = "gray") 
-    # plt.xlabel("Chasing rank") 
     plt.xlabel("Tube rank")
     plt.ylabel("Number of observations"); 
     ticklabels = [i for i in range(1, 11)]
     plt.xticks([i + 0.5 for i in range(1, 11)], ticklabels)
-    # plt.title("Chasing rank of the rich-club members")
     plt.title("Tube rank of rich-club members")
+    
+def chasingrank_vs_rc():
+    """Plots the chasing rank of rich clulb members for both cohorts"""
+    arr = np.array([2, 3, 5, 1, 2, 4, 1, 7, 4, 2, 2, 1, 1, 6, 4, 6, 5, 1, 2, 3, 2, 3, 10, 1, 2, 5, 4, 1, 2, 6, 3, 2,7 ,1 ,3,4]) # chasing rank
+    plt.hist(arr, bins = [i for i in range(1, 11)], align = 'mid', rwidth = 0.95, color = "gray") 
+    plt.xlabel("Chasing rank") 
+    plt.xlabel("Tube rank")
+    plt.ylabel("Number of observations"); 
+    ticklabels = [i for i in range(1, 11)]
+    plt.xticks([i + 0.5 for i in range(1, 11)], ticklabels)
+    plt.title("Chasing rank of the rich-club members")
     
 def chasings_vs_rc():
     tot10 = np.array([4, 24, 17, 24, 29, 30, 10]) # total chasing from RC for thres 10%
@@ -244,6 +253,8 @@ if __name__ == "__main__":
     # mutants_RC_hist()
     # rc_coefficient_histogram(4, 3)
     # chasings_vs_rc()
+    # tuberank_vs_rc()
+    chasingrank_vs_rc()
 # chasings_vs_rc_validation()
 # total_chasings_cohort()
 
