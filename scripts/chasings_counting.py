cac9f6b35c34ac0812b6ed60385f0940809ca86b
Corentin Nélias <corentin.nelias@gmail.com>
2024-09-04 12:40:48 +0200

    boxplots functions

--- scripts/chasings_counting.py
+++ scripts/chasings_counting.py
@@ -4,6 +4,7 @@ import igraph as ig
 import networkx as nx
 import os
 import sys
+import seaborn as sns
 from weighted_rc import weighted_rich_club
 
 sys.path.append('..\\src\\')
@@ -169,6 +170,81 @@ def histogram_chasing_new():
     ax.legend(title="Threshold:")
     ax.vlines([0,1,2,3,4,5,6,7,8,9,10], [0,0,0,0,0,0,0,0,0,0,0], random_chances, color = "k", label = "Random chance")  # 25 because there are 2 to 3 members of stable rich club
     # plt.savefig("C:\\Users\\Agarwal Lab\\Corentin\\Python\\clusterGUI\\plots\\chasings_vs_RC.png", dpi = 150)
+    plt.show()
+    
+def boxplot_chasing():
+    """
+    Compares the number of chasings made by RC members, mutants and others.
+    """
+    all_rc = [[0,6], [3, 8, 9], [3, 4, 8], [2, 4], [5,6], [0, 1], [3,4,6], [3, 5, 7], [6, 7, 8], [5, 8], [0, 2], [], [], [2, 8, 9], []]
+    all_mutants = [[6], [2], [6], [5], [2, 4], [7], [0, 5], [3], [2], [0,2,3], [5,7], [2,3,9], [3,9], [0,2,3], [2,3,8,9]] #took out mutants with weak histology
+    labels = ["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G10", "G11", "G12", "G13", "G14", "G15", "G16"]
+    
+    chasings_rc = [] # chasings from RC   
+    chasings_mutants = [] # chasings from RC   
+    chasings_others = [] # chasings from RC   
+
+    for idx, g in enumerate(labels):
+        data = read_graph(["C:\\Users\\Corentin offline\\Documents\\GitHub\\clusterGUI\\data\\chasing\\single\\"+g+"_single_chasing.csv"], percentage_threshold = 0)[0]
+        for rc in all_rc[idx]:
+            chasings_rc.append(np.sum(data[rc, :]))
+        for mutant in all_mutants[idx]:
+            chasings_mutants.append(np.sum(data[mutant, :]))
+        others = np.arange(10)[np.logical_and(~np.isin(np.arange(10), all_rc[idx]), ~np.isin(np.arange(10), all_mutants[idx]))]
+        for other in others:
+            chasings_others.append(np.sum(data[other, :]))
+    data = [chasings_rc, chasings_mutants, chasings_others]
+    ax = plt.axes()
+    bp = ax.boxplot(data, widths=0.6, patch_artist=True)
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    xticklabels = ["RC", "Mutants", "Others"]
+    ax.set_xticklabels(xticklabels, fontsize = 20)
+    ax.set_ylabel("Outgoing chasings", fontsize = 20)
+    colors = sns.color_palette('pastel')
+    for patch, color in zip(bp['boxes'], colors): # set colors
+        patch.set_facecolor(color)
+    plt.setp(bp['medians'], color='k')
+    bottom, top = ax.get_ylim()
+    plt.show()
+    
+def boxplot_approaches():
+    """
+    Compares the number of chasings made by RC members, mutants and others.
+    """
+    all_rc =  [[0,6], [3, 8, 9], [2, 3, 6], [2, 4], [1, 6], [0, 1], [3, 4, 6], [3, 5, 7, 8], [7, 8], [5, 8], [0, 2], [], [], [2, 8, 9], []]
+    all_mutants = [[6], [2], [6], [5], [2, 4], [7], [0, 5], [3], [2], [0,2,3], [5,7], [2,3,9], [3,9], [0,2,3], [2,3,8,9]] #took out mutants with weak histology
+    labels = ["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G10", "G11", "G12", "G13", "G14", "G15", "G16"]
+    
+    approaches_rc = [] # chasings from RC   
+    approaches_mutants = [] # chasings from RC   
+    approaches_others = [] # chasings from RC   
+
+    for idx, g in enumerate(labels):
+        data = read_graph(["..\\data\\averaged\\"+g+"\\approaches_resD7_1.csv"], percentage_threshold = 10)[0] + read_graph(["..\\data\\averaged\\"+g+"\\approaches_resD7_2.csv"], percentage_threshold = 10)[0]
+        for rc in all_rc[idx]:
+            approaches_rc.append(np.sum(data[rc, :]))
+        for mutant in all_mutants[idx]:
+            approaches_mutants.append(np.sum(data[mutant, :]))
+        others = np.arange(10)[np.logical_and(~np.isin(np.arange(10), all_rc[idx]), ~np.isin(np.arange(10), all_mutants[idx]))]
+        for other in others:
+            try:
+                approaches_others.append(np.sum(data[other, :]))
+            except:
+                pass
+    data = [approaches_rc, approaches_mutants, approaches_others]
+    ax = plt.axes()
+    bp = ax.boxplot(data, widths=0.6, patch_artist=True)
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    xticklabels = ["RC", "Mutants", "Others"]
+    ax.set_xticklabels(xticklabels, fontsize = 20)
+    ax.set_ylabel("Outgoing approaches", fontsize = 20)
+    colors = sns.color_palette('pastel')
+    for patch, color in zip(bp['boxes'], colors): # set colors
+        patch.set_facecolor(color)
+    plt.setp(bp['medians'], color='k')
+    bottom, top = ax.get_ylim()
     plt.show()
     
 def histogram_shared_passages():
@@ -390,5 +466,8 @@ if __name__ == "__main__":
     # histogram_approaches()
     # histogram_shared_passages()
     # rank_by_outgoing_chasings()
-    rank_by_outgoing_approaches()
+    # boxplot_chasing()
+    boxplot_approaches()
+
+    # rank_by_outgoing_approaches()
     
