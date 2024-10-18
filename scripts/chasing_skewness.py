89bcb9115c8eea5bb87c994b3140e8a1749fc461
Corentin Nélias <corentin.nelias@gmail.com>
2024-10-18 13:17:31 +0200

    plots

--- scripts/chasing_skewness.py
+++ scripts/chasing_skewness.py
@@ -17,43 +17,45 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
     
 def chasing_towards_piechart(normed = False, filter_big_chasers = False):
-    """
-    if both, animals are ranked by outgoing+ingoing chasings. else, they are ranked by outgoing chasings.
-    """
-    datapath = "..\\data\\chasing\\single\\"
-    # plots the rank of RC members, ranking each mice of each group by the total number of outgoing chases it 
-    # performed
-    all_rc = [[0,6], [3, 8, 9], [3, 4, 8], [5, 6], [0, 1], [3, 4, 6], [5, 7], [7, 8], [5, 8], [0, 2], [2, 8, 9]]
-    labels = ["G1", "G2", "G3","G5", "G6", "G7", "G8","G10", "G11", "G12", "G15"]
+    # """
+    # if both, animals are ranked by outgoing+ingoing chasings. else, they are ranked by outgoing chasings.
+    # """
+    # datapath = "..\\data\\chasing\\single\\"
+    # # plots the rank of RC members, ranking each mice of each group by the total number of outgoing chases it 
+    # # performed
+    # all_rc = [[0,6], [3, 8, 9], [3, 4, 8], [5, 6], [0, 1], [3, 4, 6], [5, 7], [7, 8], [5, 8], [0, 2], [2, 8, 9]]
+    # labels = ["G1", "G2", "G3","G5", "G6", "G7", "G8","G10", "G11", "G12", "G15"]
     
-    rc_chased = [] # total chasing from RC for thres 10%
+    # rc_chased = [] # total chasing from RC for thres 10%
 
-    for idx, g in enumerate(labels):
-        data = read_graph([datapath+g+"_single_chasing.csv"])[0]
-        for member in all_rc[idx]:
-            members = np.array(all_rc[idx])
-            avg_total_chasing = np.quantile(np.sum(data, axis = 1), 0.8)
-            # avg_total_chasing = np.mean(np.sum(data, axis = 1))
+    # for idx, g in enumerate(labels):
+    #     data = read_graph([datapath+g+"_single_chasing.csv"])[0]
+    #     for member in all_rc[idx]:
+    #         members = np.array(all_rc[idx])
+    #         avg_total_chasing = np.quantile(np.sum(data, axis = 1), 0.8)
+    #         # avg_total_chasing = np.mean(np.sum(data, axis = 1))
 
-            if not filter_big_chasers:
-                if (np.any(np.argmax(data[member, :]) == members)) and (np.argmax(data[member, :]) != member):
-                    rc_chased.append(1)
-                else:
-                    rc_chased.append(0)
-            elif np.sum(data[member, :]) > avg_total_chasing:
-                if (np.any(np.argmax(data[member, :]) == members)) and (np.argmax(data[member, :]) != member):
-                    rc_chased.append(1)
-                else:
-                    rc_chased.append(0)
+    #         if not filter_big_chasers:
+    #             if (np.any(np.argmax(data[member, :]) == members)) and (np.argmax(data[member, :]) != member):
+    #                 rc_chased.append(1)
+    #             else:
+    #                 rc_chased.append(0)
+    #         elif np.sum(data[member, :]) > avg_total_chasing:
+    #             if (np.any(np.argmax(data[member, :]) == members)) and (np.argmax(data[member, :]) != member):
+    #                 rc_chased.append(1)
+    #             else:
+    #                 rc_chased.append(0)
                 
 
-    total_rc_chasings = np.array(rc_chased)
-    num_chasings = len(total_rc_chasings)
-    in_frac = np.sum(total_rc_chasings)
-
-    out_frac = num_chasings - np.sum(total_rc_chasings)
+    # total_rc_chasings = np.array(rc_chased)
+    # num_chasings = len(total_rc_chasings)
+    # in_frac = np.sum(total_rc_chasings)
+    # out_frac = num_chasings - np.sum(total_rc_chasings)
+    
+    in_frac, out_frac, num_chasings = 11, 14, 25 # hand counted
+    
     in_frac = 100*(in_frac/num_chasings)
-    out_frac = 100/(out_frac/num_chasings)
+    out_frac = 100*(out_frac/num_chasings)
     if normed:
         in_frac = in_frac/2.5
         out_frac = out_frac/7.5
@@ -64,6 +66,66 @@ def chasing_towards_piechart(normed = False, filter_big_chasers = False):
     sizes = [in_frac, out_frac]
     fig, ax = plt.subplots()
     ax.pie(sizes, labels=labels, autopct='%1.1f\%%')
+    if normed:
+        ax.set_title("Direction of maximal chasings\nfrom RC members (normalized)")
+    else:
+        ax.set_title("Direction of maximal chasings\nfrom RC members")
+    plt.tight_layout()
     plt.show()
     
-chasing_towards_piechart(True, True)
+def chasing_manhanttan_plot(out = True):
+    
+    datapath = "..\\data\\chasing\\single\\"
+    # plots the rank of RC members, ranking each mice of each group by the total number of outgoing chases it 
+    # performed
+    all_rc = [[0,6], [3, 8, 9], [3, 4, 8], [5, 6], [0, 1], [3, 4, 6], [5, 7], [7, 8], [5, 8], [0, 2], [2, 8, 9]]
+    labels = ["G1", "G2", "G3", "G5", "G6", "G7", "G8","G10", "G11", "G12", "G15"]
+    
+    width = 0.05  # the width of the bars
+    x = np.arange(11)  # the label locations
+
+    fig, ax = plt.subplots(layout='constrained', figsize =  (12, 6))
+    for idx, g in enumerate(labels):
+        data = read_graph([datapath+g+"_single_chasing.csv"])[0]
+        chasings = [] 
+        colors = []
+        for member in range(len(data)):
+            try:
+                if out:
+                    chasings.append(np.sum(data[member, :]))
+                else:
+                    chasings.append(np.sum(data[:, member]))
+    
+                if np.any(member == np.array(all_rc[idx])):
+                    colors.append("green")
+                else:
+                    colors.append("blue")
+            except:
+                chasings.append(0)
+                colors.append("blue")
+        multiplier = 0
+        for i in range(len(chasings)):
+            offset = width * multiplier
+            rects = ax.bar(x[idx] + offset, chasings[i], width, label = g, color = colors[i])
+            multiplier += 1
+    
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    if out:
+        ax.set_ylabel('Total outgoing chasings', fontsize = 20)
+    else:
+        ax.set_ylabel('Total ingoing chasings', fontsize = 20)
+
+    ax.set_xticks(x + width, labels)
+    custom_handles = [
+        plt.Line2D([0], [0], color='green', lw=2, label='RC'),
+        plt.Line2D([0], [0], color='blue', lw=2, label='Non-RC')
+    ]
+    plt.legend(handles=custom_handles, fontsize = 16)    
+    plt.tight_layout()
+    plt.show()
+    
+# chasing_towards_piechart()
+chasing_manhanttan_plot(False)
+
+#plot normalized cumulative plots of chasings for ranked animals (by chasing), in and out
+# plot chasings ranking for ingoing.
