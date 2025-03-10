cf6480bcba0fe79cfbca3a5e3b86650d31f05e78
Corentin Nélias <corentin.nelias@gmail.com>
2025-03-10 09:48:53 +0100

    New plots for param stability
    Plotting day by day plots to show that overall shape of rich club does not change much under changes of parameters. For the paper G10 is probably the best example.

--- scripts/rc_in_graph_plots.py
+++ scripts/rc_in_graph_plots.py
@@ -7,18 +7,20 @@ import sys
 sys.path.append('..\\src\\')
 from read_graph import read_graph, k_core_weights, display_graph
 
-def day_to_day_interactions(dirname, mnn = 4, deg = 3):
+def day_to_day_interactions(dirname, mnn = 3, deg = 3):
     """
     Plots and saves the graph of the rich club for the interaction graph given in 'datapath'
     after mnn cuting and k-core computation. Saves on svg format.
     """
     datapath = "..\\data\\social network matrices 3days\\"+dirname+"\\"
-    fig, axs = plt.subplots(5, 1, figsize=(5, 10)) #putting all results directly on same figure
+    fig, axs = plt.subplots(5, 1, figsize=(5, 15)) #putting all results directly on same figure
     for i in range(5):
         file = "interactions_resD3_"+str(i+1)+".csv"
-        display_graph([datapath+file], axs[i], mnn = mnn, node_metric = "k-core", deg = deg, layout = "circle", node_labels = [])#, idx = [1,1,1,0,0,0,1,1,1,0])
-    fig.suptitle(dirname, fontsize=16)
-    plt.savefig("C:\\Users\\Corentin offline\\Documents\\GitHub\\clusterGUI\\plots\\params_exploration\\"+dirname+"_mnn"+str(mnn)+"_deg"+str(deg)+"_interactions.png")
+        display_graph([datapath+file], axs[i], mnn = mnn, node_metric = "rich-club", deg = deg, layout = "circle", node_labels = [])#, idx = [1,1,1,0,0,0,1,1,1,0])
+    fig.suptitle(dirname, fontsize=12)
+    #plt.savefig("C:\\Users\\Corentin offline\\Documents\\GitHub\\clusterGUI\\plots\\params_exploration\\"+dirname+"_mnn"+str(mnn)+"_deg"+str(deg)+"_interactions.png")
+    plt.savefig("C:\\Users\\wolfgang.kelsch\\Downloads\\"+dirname+"_mnn"+str(mnn)+"_deg"+str(deg)+"_interactions.svg")
+    plt.tight_layout()
     plt.show()
 
 def day_to_day_approach(dirname, mnn = 4, deg = 2, mutual = True):
@@ -65,5 +67,6 @@ def day_to_day_approachprop(dirname, mnn = 4, deg = 2):
     plt.savefig("C:\\Users\\Corentin offline\\Documents\\GitHub\\clusterGUI\\plots\\params_exploration\\"+dirname+"_mnn"+str(mnn)+"_deg"+str(deg)+"_approachprop.png")
     plt.show()
 
-for graph in ["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G10"]:
-    day_to_day_approach(graph, 4, 6, False)
+# for graph in ["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G10"]:
+#     day_to_day_approach(graph, 4, 6, False)
+day_to_day_interactions("G10", mnn = 4, deg = 4)
