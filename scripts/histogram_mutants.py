fixed issues with rich club coefficient computation

scripts/histogram_mutants.py
@@ -2,15 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from weighted_rc import weighted_rich_club
 import sys
 import os
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 os.chdir('..\\src\\')
+sys.path.append('..\\scripts\\')
 sys.path.append(os.getcwd())
 from read_graph import read_graph
+# from weighted_rc import weighted_rich_club
+from utils import get_category_indices, format_plot, add_significance
+
 from HierarchiaPy import Hierarchia
 
 datapath = "..\\data\\chasing\\single\\"
@@ -363,6 +366,106 @@ def approachRank_mutants():
     plt.ylabel(r"Count", fontsize = 15)
     plt.title(r"Appraoch rank of mutants", fontsize = 18)
     plt.show()
+    
+def chasingFraction_MutVsWT(out = True, ax = None):
+    """ 
+    Histogram of the chasing fraction for mutants vs WT mice
+    """
+    path_cohort1 = "..\\data\\reduced_data.xlsx"
+    path_cohort2 = "..\\data\\validation_cohort.xlsx"
+    chasing_dir = "..\\data\\chasing\\single\\"
+
+    approach_order_out_Mut, approach_order_in_Mut = [], []
+    approach_order_out_WT, approach_order_in_WT = [], []
+
+    # first cohort
+    df1 = pd.read_excel(path_cohort1)
+    groups1 = df1.loc[:, "group"].to_numpy()
+    Mut1 = df1.loc[:, "mutant"].to_numpy()
+    RFIDs1 = df1.loc[:, "Mouse_RFID"].to_numpy()
+
+    for group_idx in range(1, np.max(groups1)+1): #iterate over groups
+        mouse_indices = np.where(groups1 == group_idx) # find out indices of mice from current group
+        mouse_names = RFIDs1[mouse_indices]
+        approach_matrix = np.loadtxt(chasing_dir+"G"+str(group_idx)+"_single_chasing.csv",
+                                    delimiter = ",", dtype=str)[1:, 1:].astype(np.int16)
+        names_in_approach_matrix =  np.loadtxt(chasing_dir+"G"+str(group_idx)+"_single_chasing.csv",
+                                            delimiter=",", dtype=str)[0, :][1:]
+
+        chasings_out = np.sum(approach_matrix, axis = 1)
+        chasings_out = chasings_out/np.sum(chasings_out)
+        chasings_in = np.sum(approach_matrix, axis = 0)
+        chasings_in = chasings_in/np.sum(chasings_in)
+        for idx, mut in enumerate(Mut1[mouse_indices]):
+            if np.any(mouse_names[idx] == names_in_approach_matrix):
+                if mut:
+                    mut_idx = np.where(mouse_names[idx] == names_in_approach_matrix)[0][0]
+                    approach_order_out_Mut.append(list(chasings_out)[mut_idx])
+                    approach_order_in_Mut.append(list(chasings_in)[mut_idx])
+                else:
+                    wt_idx = np.where(mouse_names[idx] == names_in_approach_matrix)[0][0]
+                    approach_order_out_WT.append(list(chasings_out)[wt_idx])
+                    approach_order_in_WT.append(list(chasings_in)[wt_idx])
+
+    df2 = pd.read_excel(path_cohort2)
+    groups2 = df2.loc[:, "Group_ID"].to_numpy()
+    Mut2 = np.array(df2["genotype"] == "Oxt")
+    RFIDs2 = df2.loc[:, "Mouse_RFID"].to_numpy()
+
+    for group_idx in range(11, 18): #iterate over groups
+        mouse_indices = np.where(groups2 == group_idx) # find out indices of mice from current group
+        mouse_names = RFIDs2[mouse_indices]
+        approach_matrix = np.loadtxt(chasing_dir+"G"+str(group_idx)+"_single_chasing.csv",
+                                    delimiter = ",", dtype=str)[1:, 1:].astype(np.int16)
+        names_in_approach_matrix =  np.loadtxt(chasing_dir+"G"+str(group_idx)+"_single_chasing.csv",
+                                            delimiter=",", dtype=str)[0, :][1:]
+       
+        chasings_out = np.sum(approach_matrix, axis = 1)
+        chasings_out = chasings_out/np.sum(chasings_out)
+        chasings_in = np.sum(approach_matrix, axis = 0)
+        chasings_in = chasings_in/np.sum(chasings_in)
+        for idx, mut in enumerate(Mut2[mouse_indices]):
+            if np.any(mouse_names[idx] == names_in_approach_matrix):
+                if mut:
+                    mut_idx = np.where(mouse_names[idx] == names_in_approach_matrix)[0][0]
+                    approach_order_out_Mut.append(list(chasings_out)[mut_idx])
+                    approach_order_in_Mut.append(list(chasings_in)[mut_idx])
+                else:
+                    wt_idx = np.where(mouse_names[idx] == names_in_approach_matrix)[0][0]
+                    approach_order_out_WT.append(list(chasings_out)[wt_idx])
+                    approach_order_in_WT.append(list(chasings_in)[wt_idx])
+                      
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize = (5,4))
+
+    if out:
+        ax.hist(approach_order_out_WT, bins = np.linspace(0, 0.5, 10), rwidth= 0.9, align='mid', color = 'gray')
+        ax.hist(approach_order_out_Mut, bins = np.linspace(0, 0.5, 10), rwidth= 0.75, align='mid', color = 'darkred')
+
+    else:
+        ax.hist(approach_order_in_WT, bins = np.linspace(0, 0.5, 10), rwidth= 0.9, align='mid', color = 'gray')
+        ax.hist(approach_order_in_Mut, bins = np.linspace(0, 0.5, 10), rwidth= 0.75, align='mid', color = 'darkred')
+
+    if out:
+        ax.set_xlabel(r"Chasing fraction (outgoing)", fontsize = 17)
+    else:
+        ax.set_xlabel(r"Chasing fraction (ingoing)", fontsize = 17)
+        
+    ax.set_ylabel(r"Count", fontsize = 17)
+    ax.tick_params(axis='x', labelsize=15)
+    ax.tick_params(axis='y', labelsize=15)
+    fig.tight_layout()
+    
+    ###### ATTENTION!!! THIS BOXPLOT STATISTIC DOES NOT RESPECT THE GROUP STRUCTURE!!
+    # fig2, ax2 = plt.subplots(1, 1)
+    # data = [approach_order_out_Mut, approach_order_out_WT]
+    # bp = ax2.boxplot(data, widths=0.6, patch_artist=True, showfliers = False, zorder=1)
+    # ax2.scatter([1 + np.random.normal()*0.05 for i in range(len(data[0]))], 
+    #             data[0], label = "mutant", zorder=2); 
+    # ax2.scatter([2 + np.random.normal()*0.05 for i in range(len(data[1]))], 
+    #             data[1], label = "non-member", zorder=2); 
+    # add_significance(data, ax2, bp)
+
 
             
 if __name__ == "__main__":
@@ -373,5 +476,6 @@ if __name__ == "__main__":
     # histogram_approaches_mutants()
     # approach_order_mutants(both = True)
     # approachRank_mutants()
-    mutants_in_club_piechart()
+    # mutants_in_club_piechart()
+    chasingFraction_MutVsWT(True)
 

scripts/simple_stats.py
@@ -9,7 +9,7 @@ import os
 import sys
 import pandas as pd
 import seaborn as sns
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind, ttest_1samp
 from scipy import stats
 import seaborn as sns
 
@@ -20,7 +20,7 @@ from utils import get_category_indices, format_plot, add_significance
 datapath = "..\\data\\chasing\\single\\"
 datapath = "..\\data\\averaged\\"
 #plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
+plt.rcParams["font.family"] = "Arial"
 
 
 path = "..\\data\\reduced_data.xlsx"
@@ -537,15 +537,15 @@ def tube_rank(sep = False, all_wt = False):
 
     alpha, size = 0.5, 30
     # ax.scatter([1 + np.random.normal()*0.05 for i in range(len(data[0]))], 
-    #             data[0], alpha = alpha, s = size, label = "mutant", zorder=2); 
+                # data[0], alpha = alpha, s = size, label = "mutant", zorder=2); 
     # ax.scatter([2 + np.random.normal()*0.05 for i in range(len(data[1]))], 
-    #             data[1], alpha = alpha, s = size, label = "non-member", zorder=2); 
+                # data[1], alpha = alpha, s = size, label = "non-member", zorder=2); 
     
-    plt.hist(ranks_wt, bins = 10, color = "green", alpha = alpha, density=False, rwidth = 0.95)
-    plt.hist(ranks_mutants, bins = 10, color = "red", alpha = alpha, density=False, rwidth=0.95)
+    plt.hist(ranks_wt, bins = 10, color = "gray", density=False, rwidth = 0.9)
+    plt.hist(ranks_mutants, bins = 10, color = "darkred", density=False, rwidth=0.75)
+    ax.set_xlabel("Tube rank", fontsize = 20)
+    ax.set_ylabel("Count", fontsize = 20)
 
-    
-    ax.set_ylabel("Tube rank", fontsize = 20)
     # format_plot(ax, bp, xticklabels = ["Mutants", "WT"]) # set x_axis, and colors of each bar
     plt.tight_layout()
     plt.show()

scripts/weighted_rc_coeff.py
@@ -6,7 +6,7 @@ import os
 from tqdm import tqdm
 from copy import deepcopy
 from scipy.stats import sem
-
+from scipy.stats import ttest_ind, ttest_1samp
 
 def mnn_cut(arr, nn = 2):
     """
@@ -30,76 +30,174 @@ def mnn_cut(arr, nn = 2):
                 mnn_arr[j, i] += arr[j, i]
     return mnn_arr
 
-def richness_from_data(arr, k):
-    """
-    This is a custom function to compute the weighted richness of the graph.
-    We cannot use the k_core in this case as it requires interconnectedness. Instead,
-    we use the definition of richness (see paper on rich clubs)
-    """
-    richness = []
-    for i in range(arr.shape[0]):
-        if np.sum(arr[i, :] > 0) >= k: #is this node rich?
-            richness.append(np.sum(arr[i, :]))
-    return np.sum(np.array(richness))
+# def richness_from_data(arr, k):
+#     """
+#     This is a custom function to compute the weighted richness of the graph.
+#     We cannot use the k_core in this case as it requires interconnectedness. Instead,
+#     we use the definition of richness (see paper on rich clubs)
+#     """
+#     richness = []
+#     for i in range(arr.shape[0]):
+#         if np.sum(arr[i, :] > 0) >= k: #is this node rich?
+#             richness.append(np.sum(arr[i, :]))
+#     return np.sum(np.array(richness))
 
+def richness_from_data(g, k):
+    """Returns sum of edge weights where both endpoints have degree > k"""
+    # deg_source = np.array([g.degree(e.source) for e in g.es])
+    # deg_target = np.array([g.degree(e.target) for e in g.es])
+    # weights = np.array(g.es["weight"])
+    # mask = (deg_source >= k) & (deg_target >= k)
+    # return np.sum(weights[mask])
+    return 2*sum(e['weight'] for e in g.es 
+                      if g.degree(e.source) >= k and g.degree(e.target) >= k)
+    # return 2*sum(1 for e in g.es 
+    #                   if g.degree(e.source) >= k and g.degree(e.target) >= k)
 
-def weighted_rich_club(data, k = 3, iterations = 100):
-    observed_richness = richness_from_data(data, k)
+def random_richness(data, k, iterations = 100):
     random_richness = []
-    for i in tqdm(range(iterations)):
-        ## shuffling array while keeping the symmetric structure of the array
-        values = np.triu(data, 1)
-        idx = np.triu_indices(data.shape[0], 1, data.shape[1])
-        shuffled_values = values[idx]
-        np.random.shuffle(shuffled_values)
-        shuffled_data = np.zeros(data.shape)
-        counter = 0
-        for m in range(data.shape[0]):
-            for n in range(m, data.shape[0]):
-                if m != n:
-                    shuffled_data[m, n] = shuffled_values[counter]
-                    shuffled_data[n, m] = shuffled_values[counter]
-                    counter += 1
-        random_richness.append(richness_from_data(shuffled_data, k))
-        
-    normalization_factor = np.sum(np.array(random_richness))/iterations
-    return observed_richness/normalization_factor
+    for i in range(iterations):
+        # cut_arr = (mnn_cut(data, k) > 0)*1
+        cut_arr = mnn_cut(data, k)
+        g_rand = ig.Graph.Weighted_Adjacency(cut_arr, mode='undirected')
+        original_weights = g_rand.es['weight']
+        g_rand.rewire(mode="simple", n=1000)
+        shuffled_weights = original_weights.copy()
+        np.random.shuffle(shuffled_weights)
+        g_rand.es['weight'] = shuffled_weights
+        cut_shuffled_arr = np.array(g_rand.get_adjacency(attribute="weight"))
+        # n = data.shape[0]
+        # triu_indices = np.triu_indices(n, k=1)  # k=1 excludes diagonal
+        # upper_values = data[triu_indices]
+        # np.random.shuffle(upper_values)
+        # shuffled_data = np.zeros_like(data)
+        # shuffled_data[triu_indices] = upper_values          
+        # shuffled_data.T[triu_indices] = upper_values  
+        # cut_shuffled_arr = (mnn_cut(shuffled_data, k) > 0)*1
+        where_rand_rc = np.where(np.sum(cut_shuffled_arr > 0, 0) >= k)
+        rand_normalization = np.sum(cut_shuffled_arr[where_rand_rc, :])
+        # g_rand = ig.Graph.Weighted_Adjacency(cut_shuffled_arr, mode='undirected')
+        # rand_richness = richness_from_data(g_rand, k)
+        random_richness.append(richness_from_data(g_rand, k)/rand_normalization)
+    rand_richnesses = np.array(random_richness)
+    return np.sum(rand_richnesses[np.where(rand_richnesses != np.inf)])/iterations
+
+def weighted_rich_club(data, k = 3, iterations = 50):
+    # observed_richness = richness_from_data(data, k)
+    # cut_arr = (mnn_cut(data, k) > 0)*1
+    cut_arr = mnn_cut(data, k)
+    where_rc = np.where(np.sum(cut_arr > 0, 0) >= k)
+    normalization = np.sum(cut_arr[where_rc, :])
+    g = ig.Graph.Weighted_Adjacency(cut_arr, mode='undirected')
+    observed_richness = richness_from_data(g, k)/normalization
+
+    rand_richness = random_richness(data, k, iterations)
+    return observed_richness/rand_richness
 
 labels = ["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G10", "G11", "G12", "G13", "G14", "G15", "G16", "G17"]
-def rich_club_coefficients(variable, k, window = 3):
+def rc_coefficients_all(variable, k, window = 3):
+    """
+    Plots the rc coefficients of all groups, at the expense of showing the distribution of value (non notion of significance.)
+    """
+    all_coeffs = []
     avg_rc_coeffs, std_rc_coeffs = [], []
-    for day in np.arange(1, 16//window):
+    fig, ax = plt.subplots(1, 1)
+    for day in tqdm(np.arange(1, 15//window+1)):
         coefficients = []
         for graph_idx in range(len(labels)):
             try:
                 path_to_file = f"..\\data\\both_cohorts_{window}days\\"+labels[graph_idx]+"\\"+variable+f"_resD{window}_"+str(day)+".csv"
                 arr = np.loadtxt(path_to_file, delimiter=",", dtype=str)
                 data = arr[1:, 1:].astype(float)
-                data = mnn_cut(data, k)
-                rc = weighted_rich_club(data, k, 100)
+                # data = mnn_cut(data, k)
+                rc = weighted_rich_club(data, k, 50)
                 coefficients.append(rc)
             except Exception as e:
                 print(e)
+        all_coeffs.append(coefficients)
         coefficients = np.array(coefficients)
         avg_rc_coeffs.append(np.nanmean(coefficients))
-        std_rc_coeffs.append(sem(coefficients[~np.isnan(coefficients)]))
-    t = np.arange(1, 16//window)
+        std_rc_coeffs.append(np.nanstd(coefficients[~np.isnan(coefficients)]))
+        plt.scatter([day]*len(coefficients), coefficients, alpha = 0.5, s = 100, color = "k")
+        plt.boxplot(coefficients[~np.isnan(coefficients)], positions = [day], showfliers= False)
+        t_statistic, p_value = ttest_1samp(coefficients[~np.isnan(coefficients)], popmean=1, alternative='greater') 
+        print(p_value)
+        if p_value < 0.001:
+            plt.text(day, 2.2, "***", fontsize="20",horizontalalignment='center')
+        elif p_value < 0.01:
+            plt.text(day, 2.2, "**", fontsize="20",horizontalalignment='center')
+        elif p_value < 0.05:
+            plt.text(day, 2.2, "*", fontsize="20",horizontalalignment='center')
+
+
+    ax.spines[['right', 'top']].set_visible(False)   
+    t = np.arange(1, 15//window+1)
     avg_rc_coeffs, std_rc_coeffs = np.array(avg_rc_coeffs), np.array(std_rc_coeffs)
-    plt.figure(figsize=(4, 3.5))
-    plt.plot(t, avg_rc_coeffs, '-o', lw = 2, c = "k", label = "Average across groups")
-    # plt.scatter(t, avg_rc_coeffs, lw = 2, c = "k", label = "Rich club coefficient")
-    plt.fill_between(t, avg_rc_coeffs - std_rc_coeffs, avg_rc_coeffs + std_rc_coeffs, color="k", alpha=0.2)
+    # plt.figure(figsize=(4, 3.5))
+    # for line in range(len(all_coeffs[0])):
+    #     plt.plot(t, [all_coeffs[idx][line] for idx in range(len(all_coeffs))])
+    plt.plot(t, avg_rc_coeffs, '-o', lw = 2, c = "gray", label = "Average across groups")
+    # # plt.scatter(t, avg_rc_coeffs, lw = 2, c = "k", label = "Rich club coefficient")
+    # plt.fill_between(t, avg_rc_coeffs - std_rc_coeffs, avg_rc_coeffs + std_rc_coeffs, color="k", alpha=0.2)
     plt.hlines(1, t[0], t[-1], ls = "--", color = "k", label = "Randomized graph")
     plt.xlabel("Day", fontsize = 18)
     plt.ylabel("Richness", fontsize = 18)
-    plt.xticks(range(1, 16//window), fontsize = 14) 
-    plt.yticks(np.linspace(1, 3, 3), fontsize = 14)
-    plt.ylim(0.5, 3)
+    # plt.xticks(range(1, 16//window), fontsize = 14) 
+    plt.yticks(np.linspace(0, 2.5, 6), fontsize = 14)
+    # plt.ylim(0.3, 2.5)
     plt.legend()
     plt.tight_layout()
     
     return avg_rc_coeffs, std_rc_coeffs
 
+
+def rc_coefficient_selected(group, variable, k, window = 3):
+    plt.figure()
+    all_coeffs = []
+    avg_rc_coeffs, sem_rc_coeffs = [], []
+    print(labels[group])
+    for day in tqdm(np.arange(1, 15//window+1)):
+        coefficients = []
+        try:
+            path_to_file = f"..\\data\\both_cohorts_{window}days\\"+labels[group]+"\\"+variable+f"_resD{window}_"+str(day)+".csv"
+            arr = np.loadtxt(path_to_file, delimiter=",", dtype=str)
+            data = arr[1:, 1:].astype(float)
+            for i in range(100):
+                rc = weighted_rich_club(data, k, 1)
+                coefficients.append(rc)
+        except Exception as e:
+            print(e)
+        all_coeffs.append(coefficients)
+        coefficients = np.array(coefficients)
+        med_val = np.nanmedian(coefficients) if np.nanmedian(coefficients) != np.inf else 0
+        avg_rc_coeffs.append(med_val)
+        SEM = sem(coefficients[~np.isnan(coefficients)])
+        SEM = SEM if (~np.isinf(SEM) and ~np.isnan(SEM)) else 0
+        sem_rc_coeffs.append(SEM)
+        # plt.scatter([day]*len(coefficients), coefficients, alpha = 0.5, s = 100, color = "k")
+      #  plt.boxplot(coefficients, positions = [day], showfliers= False)
+        
+    t = np.arange(1, 15//window+1)
+    avg_rc_coeffs, sem_rc_coeffs = np.array(avg_rc_coeffs), np.array(sem_rc_coeffs)
+    # plt.figure(figsize=(4, 3.5))
+    # for line in range(len(all_coeffs[0])):
+    #     plt.plot(t, [all_coeffs[idx][line] for idx in range(len(all_coeffs))])
+    plt.plot(t, avg_rc_coeffs, '-o', lw = 2, c = "gray", label = "Median value")
+    # # plt.scatter(t, avg_rc_coeffs, lw = 2, c = "k", label = "Rich club coefficient")
+    plt.fill_between(t, avg_rc_coeffs - sem_rc_coeffs, avg_rc_coeffs + sem_rc_coeffs, color="k", alpha=0.2)
+    plt.hlines(1, t[0], t[-1], ls = "--", color = "k", label = "Randomized graph")
+    plt.xlabel("Day", fontsize = 18)
+    plt.ylabel("Richness", fontsize = 18)
+    plt.xticks(range(1, 16//window + 1), fontsize = 14) 
+    # plt.yticks(np.linspace(1, 3, 3), fontsize = 14)
+    # plt.ylim(0.5, 2)
+    plt.legend()
+    plt.tight_layout()
+    
+    return avg_rc_coeffs, sem_rc_coeffs
+
 if __name__ == "__main__":
-    a, s = rich_club_coefficients('interactions', 5, 3)
+    a, s = rc_coefficients_all('interactions', 3, 3)
+
+    # a, s = rc_coefficient_selected(5, 'interactions', 3, 3)
     